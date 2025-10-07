7fb24975e061380adca8cf082bb41eb62afa8442
mur3l <tippitauno@gmail.com>
2025-10-07 13:24:17 +0300

    TumeKaust_Project COMMIT

---
+++ TumeKaust/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace TumeKaust
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
