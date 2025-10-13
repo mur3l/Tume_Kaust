268289b350fa70770dff3b7e190b2e8b8444a1c8
GervinAnsi <158846288+GervinAnsi@users.noreply.github.com>
2025-10-13 14:51:01 +0300

    Mainpage view, layout and styles

--- TumeKaust/MainPage.xaml.cs
+++ TumeKaust/MainPage.xaml.cs
@@ -6,7 +6,15 @@
         public MainPage()
         {
             InitializeComponent();
+
+            FadeInImage();
         }
 
+        private async void FadeInImage()
+        {
+            // ootab 1 sekundi ja animatsioon kaivitub viieks sekundiks
+            await Task.Delay(1000);
+            await logoimage.FadeTo(1, 5000); // 0 → 1 opacity, 5000ms
+        }
     }
 }
