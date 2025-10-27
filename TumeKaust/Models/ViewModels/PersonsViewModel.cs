ac2af2af0c979c548ecefb578fda21312cf8e53f
JelenaPenkina <122533981+JelenaPenkina@users.noreply.github.com>
2025-10-27 11:41:40 +0200

    Täiendused mergemisel

--- TumeKaust/Models/ViewModels/PersonsViewModel.cs
+++ TumeKaust/Models/ViewModels/PersonsViewModel.cs
@@ -4,7 +4,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using TumeKaust.Models.Persons;
+using TumeKaust.Models;
 
 namespace TumeKaust.Models.ViewModels
 {
